372f0f54bfbc0b3f1b61f640d0b492965907e8be
Sabaa <Sabaa>
2024-08-05 14:05:37 +0300

    test

---
+++ AmitApplication/src/com/jbs/a/A.java
@@ -0,0 +1,37 @@
+package com.jbs.a;
+
+public class A {
+
+	private String name;
+	protected String nameA;
+	public String nameB;
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNameA() {
+		return nameA;
+	}
+
+	public void setNameA(String nameA) {
+		this.nameA = nameA;
+	}
+
+	public String getNameB() {
+		return nameB;
+	}
+
+	public void setNameB(String nameB) {
+		this.nameB = nameB;
+	}
+
+	
+	
+	
+}
